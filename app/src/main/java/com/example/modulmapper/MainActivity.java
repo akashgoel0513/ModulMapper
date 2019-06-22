0b2676e85e37494be02ec664a40f8c5e2aeda7ed
MyEmployees6 <51468993+MyEmployees6@users.noreply.github.com>
2019-06-23 00:28:58 +1200

    ModuleMapper

--- app/src/main/java/com/example/modulmapper/MainActivity.java
+++ app/src/main/java/com/example/modulmapper/MainActivity.java
@@ -9,11 +9,13 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button student_btn, client_btn;
+    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db=new DatabaseHelper(this);
 
         student_btn = (Button)findViewById(R.id.student_btn);
         client_btn = (Button)findViewById(R.id.client_btn);
@@ -32,5 +34,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        db.addCourses();
+        db.addNetworkSem1();
+        db.addNetworkSem2();
+        db.addNetworkSem3();
+        db.addNetworkSem4();
+        db.addNetworkSem5();
+        db.addNetworkSem6();
+        db.addDBSem1();
+        db.addDBSem2();
+        db.addDBSem3();
+        db.addDBSem4();
+        db.addDBSem5();
+        db.addDBSem6();
+        db.addWEBSem1();
+        db.addWEBSem2();
+        db.addWEBSem3();
+        db.addWEBSem4();
+        db.addWEBSem5();
+        db.addWEBSem6();
+        db.addSoftwareSem1();
+        db.addSoftwareSem2();
+        db.addSoftwareSem3();
+        db.addSoftwareSem4();
+        db.addSoftwareSem5();
+        db.addSoftwareSem6();
     }
 }
